468a00b98119171de97f38bc4112a2c17fd13390
SuxrobAbdumalikov <suxrobabdumalikov99@gmail.com>
2023-08-23 01:40:36 +0500

    install main section almost finished only background little images left

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
 axios
   .get('https://jsonplaceholder.typicode.com/todos/1')
   .then((data) => console.log(data.data))
+  //=========>> Main section Swiper <<================//
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
