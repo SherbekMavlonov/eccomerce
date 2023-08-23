98a44c0f5a63a6f303ef2c92d7fdf266a1efef7b
Azimsher07 <Azimsher070@gmail.com>
2023-08-23 14:30:27 +0500

    swiper ishlatish kerak

---
+++ js/topCategories.js
@@ -0,0 +1,14 @@
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js";
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
