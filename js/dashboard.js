001d8acc7400f0676de9ea28f2872be9eb449d99
SuxrobAbdumalikov <suxrobabdumalikov99@gmail.com>
2023-08-24 04:59:50 +0500

    account setting full finished with js of dashboard section

--- js/dashboard.js
+++ js/dashboard.js
@@ -36,6 +36,75 @@ document.getElementById("defaultOpen").click();
 /* === Shopping Cart content end === */
 
 /* === Settings content start === */
+const input = document.querySelector("#uploadImgInput");
+const showImage = document.querySelector("#showImage");
+const accountSettingsBtn = document.querySelector("#accountSettingsBtn");
+const fNameInput = document.querySelector("#fNameInput");
+const lNameInput = document.querySelector("#lNameInput");
+const emailInput = document.querySelector("#emailInput");
+const phoneNumberInput = document.querySelector("#phoneNumberInput");
+const form = document.querySelector("form");
+
+//=====>> save images in localStorage <<===========//
+input.addEventListener("change", (e) => {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    console.log(reader.result);
+    localStorage.setItem("settingsImg", reader.result);
+  });
+  reader.readAsDataURL(e.target.files[0]);
+});
+
+//========>> account settings section <<================//
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const fname = localStorage.setItem('fname',fNameInput.value);
+  const lname = localStorage.setItem('lname',lNameInput.value);
+  const email = JSON.stringify(localStorage.setItem('email',emailInput.value));
+  const number = localStorage.setItem('number',phoneNumberInput.value);
+  console.log(fname);
+  console.log(lname);
+  console.log(email);
+  console.log(number);
+
+  // const formData = new FormData(form);
+  // const object = Object.fromEntries(formData);
+  // console.log(object);
+  // const json = JSON.stringify(object);
+  // localStorage.setItem("accountSettings", json);
+  // const api = fetch(`https://httpbin.org/post`, {
+  //   method: "POST",
+  //   body: json,
+  //   headers: {
+  //     "Content-type": "application/json",
+  //   },
+  // })
+  //   .then((res) => res.json())
+  //   .then((data) => console.log(data));
+});
+
+//=====>> take account settings section from localStorage <<===========//
+document.addEventListener("DOMContentLoaded", () => {
+  const settingsImg = localStorage.getItem("settingsImg");
+  if (settingsImg) {
+    showImage.setAttribute("src", settingsImg);
+  }
+});
+
+// const accountJson = localStorage.getItem("accountSettings");
+// const jsonParsed = JSON.parse(accountJson);
+fNameInput.value = localStorage.getItem('fname');
+lNameInput.value = localStorage.getItem('lname');
+emailInput.value = localStorage.getItem('email')
+phoneNumberInput.value = localStorage.getItem('number');
+
+//=====>> take images from file <<===============//
+input.addEventListener("change", (e) => {
+  const file = e.target.files[0];
+  let inputImage = document.querySelector("input[type=file]");
+  inputImage.files[0];
+  showImage.src = URL.createObjectURL(file);
+});
 /* === Settings content end === */
 
 /* === Log out content start === */
