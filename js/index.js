0fe571ed6593e77a032371213911b5a148600b14
Azimsher07 <Azimsher070@gmail.com>
2023-08-23 17:40:33 +0500

    Categories added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
 axios
-  .get('https://jsonplaceholder.typicode.com/todos/1')
-  .then((data) => console.log(data.data))
+  .get("https://jsonplaceholder.typicode.com/todos/1")
+  .then((data) => console.log(data.data));
+
+// Azimsher
+
+new Swiper(".topCatigoriesSwiper", {
+  slidesPerView: 6,
+  spaceBetween: 24,
+  loop: true,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".next",
+    prevEl: ".prev",
+  },
+});
+
+// Azimsher
