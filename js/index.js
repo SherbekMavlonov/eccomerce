95c92f6f54963fcc6e5aa26729a58c456fcf88fd
Dilshodbek <dilshodjonaqiyev@gmail.com>
2023-08-24 19:37:13 +0500

    added js

--- js/index.js
+++ js/index.js
@@ -1,238 +1,233 @@
-import Swiper from "../node_modules/swiper/swiper-bundle.mjs";
-import { TESTOMONIAL } from "./__mock__data.js";
+import Swiper from "../node_modules/swiper/swiper-bundle.mjs"
+import { TESTOMONIAL } from "./__mock__data.js"
 
 // ---> samandar
 
 function bannerTimer() {
-  const bannerBoxes = document.querySelectorAll("#Banner .banner-box");
-  const startingAtPrice = document.querySelector(".startingAtPrice");
-  const bannerDay = document.querySelector("#Banner .day");
-  const bannerHour = document.querySelector("#Banner .hour");
-  const bannerMinute = document.querySelector("#Banner .minute");
-  const bannerSecond = document.querySelector("#Banner .second");
+    const bannerBoxes = document.querySelectorAll("#Banner .banner-box")
+    const startingAtPrice = document.querySelector(".startingAtPrice")
+    const bannerDay = document.querySelector("#Banner .day")
+    const bannerHour = document.querySelector("#Banner .hour")
+    const bannerMinute = document.querySelector("#Banner .minute")
+    const bannerSecond = document.querySelector("#Banner .second")
 
-  let box1Img = "vegetables-img.jpg";
-  let box2Img = "vegetables-sale-img.jpg";
-  bannerBoxes[0].style.background = `linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.35)),
-  url('../images/${box1Img}')`;
+    let box1Img = "vegetables-img.jpg"
+    let box2Img = "vegetables-sale-img.jpg"
+    bannerBoxes[0].style.background = `linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.35)),
+  url('../images/${box1Img}')`
 
-  bannerBoxes[1].style.background = `linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.35)),
-  url('../images/${box2Img}')`;
-  bannerBoxes.forEach((box) => (box.style.backgroundSize = "cover"));
+    bannerBoxes[1].style.background = `linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.35)),
+  url('../images/${box2Img}')`
+    bannerBoxes.forEach((box) => (box.style.backgroundSize = "cover"))
 
-  let d = 2;
-  let h = 16;
-  let m = 42;
-  let s = 25;
+    let d = 2
+    let h = 16
+    let m = 42
+    let s = 25
 
-  let allTime = d * 24 * 3600 + h * 3600 + m * 60 + s;
-  let stopped = null;
+    let allTime = d * 24 * 3600 + h * 3600 + m * 60 + s
+    let stopped = null
 
-  bannerDay.textContent = `${d < 10 ? "0" + d : d}`;
-  bannerHour.textContent = `${h < 10 ? "0" + h : h}`;
-  bannerMinute.textContent = `${m < 10 ? "0" + m : m}`;
-  bannerSecond.textContent = `${s < 10 ? "0" + s : s}`;
+    bannerDay.textContent = `${d < 10 ? "0" + d : d}`
+    bannerHour.textContent = `${h < 10 ? "0" + h : h}`
+    bannerMinute.textContent = `${m < 10 ? "0" + m : m}`
+    bannerSecond.textContent = `${s < 10 ? "0" + s : s}`
 
-  stopped = setInterval(() => {
-    if (s < 0 && m == 0 && h == 0 && d == 0) {
-      return clearInterval(stopped);
-    }
-    if (s == 0 && m == 0 && h == 0 && d - 1 >= 0) {
-      d = d - 1;
-      h = 23;
-      m = 59;
-      s = 59;
-      bannerDay.textContent = `${d < 10 ? "0" + d : d}`;
-      bannerHour.textContent = `${h < 10 ? "0" + h : h}`;
-      bannerMinute.textContent = `${m < 10 ? "0" + m : m}`;
-      bannerSecond.textContent = `${s < 10 ? "0" + s : s}`;
-    }
-    if (s == 0 && m == 0 && h - 1 >= 0) {
-      h = h - 1;
-      m = 59;
-      s = 59;
-      bannerHour.textContent = `${h < 10 ? "0" + h : h}`;
-      bannerMinute.textContent = `${m < 10 ? "0" + m : m}`;
-      bannerSecond.textContent = `${s < 10 ? "0" + s : s}`;
-    }
-    if (s == 0 && m - 1 >= 0) {
-      m = m - 1;
-      s = 59;
-      bannerMinute.textContent = `${m < 10 ? "0" + m : m}`;
-    }
-    bannerSecond.textContent = `${s < 10 ? "0" + s : s}`;
-    s--;
-  }, 1000);
+    stopped = setInterval(() => {
+        if (s < 0 && m == 0 && h == 0 && d == 0) {
+            return clearInterval(stopped)
+        }
+        if (s == 0 && m == 0 && h == 0 && d - 1 >= 0) {
+            d = d - 1
+            h = 23
+            m = 59
+            s = 59
+            bannerDay.textContent = `${d < 10 ? "0" + d : d}`
+            bannerHour.textContent = `${h < 10 ? "0" + h : h}`
+            bannerMinute.textContent = `${m < 10 ? "0" + m : m}`
+            bannerSecond.textContent = `${s < 10 ? "0" + s : s}`
+        }
+        if (s == 0 && m == 0 && h - 1 >= 0) {
+            h = h - 1
+            m = 59
+            s = 59
+            bannerHour.textContent = `${h < 10 ? "0" + h : h}`
+            bannerMinute.textContent = `${m < 10 ? "0" + m : m}`
+            bannerSecond.textContent = `${s < 10 ? "0" + s : s}`
+        }
+        if (s == 0 && m - 1 >= 0) {
+            m = m - 1
+            s = 59
+            bannerMinute.textContent = `${m < 10 ? "0" + m : m}`
+        }
+        bannerSecond.textContent = `${s < 10 ? "0" + s : s}`
+        s--
+    }, 1000)
 }
 bannerTimer()
 // samandar <----
 
 //=========>> Main section Swiper <<================//
 var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 30,
-  centeredSlides: true,
-  loop: true,
-  speed: 1600,
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    speed: 1600,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+})
 
 // Kamroncik
-const likeIcon = document.querySelector(".product_offerLike");
+const likeIcon = document.querySelector(".product_offerLike")
 
 likeIcon.addEventListener("click", () => {
-  likeIcon.innerHTML = `
+    likeIcon.innerHTML = `
   <style>
       .product_offerLike img{
           color:red;
       }
   </style>
-  `;
-});
+  `
+})
 // Kamroncik
 // Savbatov Asadbek
 
-let valueDisplays = document.querySelectorAll(".num");
-let interval = 4000;
+let valueDisplays = document.querySelectorAll(".num")
+let interval = 4000
 
 valueDisplays.forEach((valueDisplay) => {
-  let startValue = 0;
-  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
-  let duration = Math.floor(interval / endValue);
-  let counter = setInterval(function () {
-    // startValue += 1;
-    // valueDisplay.textContent = startValue;
+    let startValue = 0
+    let endValue = parseInt(valueDisplay.getAttribute("data-val"))
+    let duration = Math.floor(interval / endValue)
+    let counter = setInterval(function () {
+        // startValue += 1;
+        // valueDisplay.textContent = startValue;
 
-    if (startValue < 1000) {
-      startValue += 5;
-      valueDisplay.textContent = startValue;
-    }
+        if (startValue < 1000) {
+            startValue += 5
+            valueDisplay.textContent = startValue
+        }
 
-    if (startValue >= 1000) {
-      startValue += 100;
-      valueDisplay.textContent = (startValue / 1000).toFixed(1) + "K+";
-    }
+        if (startValue >= 1000) {
+            startValue += 100
+            valueDisplay.textContent = (startValue / 1000).toFixed(1) + "K+"
+        }
 
-    if (startValue >= 10000) {
-      startValue += 5000;
-    }
+        if (startValue >= 10000) {
+            startValue += 5000
+        }
 
-    if (startValue >= 1000000) {
-      startValue += 500000;
-      valueDisplay.textContent = (startValue / 1000000).toFixed(1) + "M+";
-    }
+        if (startValue >= 1000000) {
+            startValue += 500000
+            valueDisplay.textContent = (startValue / 1000000).toFixed(1) + "M+"
+        }
 
-    if (startValue >= endValue) {
-      if (startValue < 1000) {
-        valueDisplay.textContent = endValue;
-      }
+        if (startValue >= endValue) {
+            if (startValue < 1000) {
+                valueDisplay.textContent = endValue
+            }
 
-      if (startValue >= 1000) {
-        valueDisplay.textContent =
-          endValue % 1000 === 0 ? endValue / 1000 + "K+" : (endValue / 1000).toFixed(1) + "K+";
-      }
+            if (startValue >= 1000) {
+                valueDisplay.textContent =
+                    endValue % 1000 === 0 ? endValue / 1000 + "K+" : (endValue / 1000).toFixed(1) + "K+"
+            }
 
-      if (startValue >= 1000000) {
-        valueDisplay.textContent =
-          endValue % 1000000 === 0
-            ? endValue / 1000000 + "M+"
-            : (endValue / 1000000).toFixed(1) + "M+";
-      }
-      clearInterval(counter);
-    }
-  }, duration);
-});
+            if (startValue >= 1000000) {
+                valueDisplay.textContent =
+                    endValue % 1000000 === 0 ? endValue / 1000000 + "M+" : (endValue / 1000000).toFixed(1) + "M+"
+            }
+            clearInterval(counter)
+        }
+    }, duration)
+})
 
 // Savbatov Asadbek
 
 // Testamonial JavaScript
+;(() => {
+    const swiperWrapper = document.querySelector("#testimonials .testomonialSwiperContainer .swiper-wrapper")
 
-(() => {
-  const swiperWrapper = document.querySelector(
-    "#testimonials .testomonialSwiperContainer .swiper-wrapper"
-  );
+    const testamonialSwiper = new Swiper(".swiper-container", {
+        loop: true,
+        loopedSlides: 3,
+        slidesPerView: 3,
+        navigation: {
+            nextEl: ".testamonialScrollRight",
+            prevEl: ".testamonialScrollLeft",
+        },
+        autopay: {
+            enabled: true,
+            delay: 5000,
+        },
+    })
 
-  const testamonialSwiper = new Swiper(".swiper-container", {
-    loop: true,
-    loopedSlides: 3,
-    slidesPerView: 3,
-    navigation: {
-      nextEl: ".testamonialScrollRight",
-      prevEl: ".testamonialScrollLeft",
-    },
-    autopay: {
-      enabled: true,
-      delay: 5000,
-    },
-  });
+    TESTOMONIAL.forEach((customer) => {
+        const testomonial = document.createElement("div")
+        const tilda = document.createElement("div")
+        const imgTilda = document.createElement("img")
+        const content = document.createElement("div")
+        const clientDiv = document.createElement("div")
+        const reviewer = document.createElement("div")
+        const rating = document.createElement("div")
+        const reviewerImageDiv = document.createElement("div")
+        const info = document.createElement("div")
+        const reviewerImage = document.createElement("img")
+        const customersName = document.createElement("div")
+        const customerRole = document.createElement("div")
+        const ratingImg = document.createElement("img")
 
-  TESTOMONIAL.forEach((customer) => {
-    const testomonial = document.createElement("div");
-    const tilda = document.createElement("div");
-    const imgTilda = document.createElement("img");
-    const content = document.createElement("div");
-    const clientDiv = document.createElement("div");
-    const reviewer = document.createElement("div");
-    const rating = document.createElement("div");
-    const reviewerImageDiv = document.createElement("div");
-    const info = document.createElement("div");
-    const reviewerImage = document.createElement("img");
-    const customersName = document.createElement("div");
-    const customerRole = document.createElement("div");
-    const ratingImg = document.createElement("img");
+        imgTilda.setAttribute("src", "../images/icons/tilda-icon.svg")
+        imgTilda.setAttribute("alt", "icon")
+        imgTilda.setAttribute("loading", "lazy")
+        reviewerImage.setAttribute("src", customer.customerImage)
+        reviewerImage.setAttribute("alt", "icon")
+        reviewerImage.setAttribute("loading", "lazy")
+        ratingImg.setAttribute("src", "../images/icons/star-full.svg")
+        ratingImg.setAttribute("alt", "icon")
+        ratingImg.setAttribute("loading", "lazy")
 
-    imgTilda.setAttribute("src", "../images/icons/tilda-icon.svg");
-    imgTilda.setAttribute("alt", "icon");
-    imgTilda.setAttribute("loading", "lazy");
-    reviewerImage.setAttribute("src", customer.customerImage);
-    reviewerImage.setAttribute("alt", "icon");
-    reviewerImage.setAttribute("loading", "lazy");
-    ratingImg.setAttribute("src", "../images/icons/star-full.svg");
-    ratingImg.setAttribute("alt", "icon");
-    ratingImg.setAttribute("loading", "lazy");
+        testomonial.classList.add("swiper-slide", "testomonial")
+        tilda.classList.add("tilda")
+        content.classList.add("content")
+        clientDiv.classList.add("client")
+        reviewer.classList.add("reviewer")
+        rating.classList.add("rating")
+        reviewerImageDiv.classList.add("reviewerImage")
+        info.classList.add("info")
+        customersName.classList.add("customerName")
+        customerRole.classList.add("customer")
+        customersName.textContent = customer.customerName
+        customerRole.textContent = "Customer"
 
-    testomonial.classList.add("swiper-slide", "testomonial");
-    tilda.classList.add("tilda");
-    content.classList.add("content");
-    clientDiv.classList.add("client");
-    reviewer.classList.add("reviewer");
-    rating.classList.add("rating");
-    reviewerImageDiv.classList.add("reviewerImage");
-    info.classList.add("info");
-    customersName.classList.add("customerName");
-    customerRole.classList.add("customer");
-    customersName.textContent = customer.customerName;
-    customerRole.textContent = "Customer";
+        testomonial.appendChild(tilda)
+        testomonial.appendChild(content)
+        testomonial.appendChild(clientDiv)
+        tilda.appendChild(imgTilda)
+        content.textContent = customer.content
+        clientDiv.appendChild(reviewer)
+        clientDiv.appendChild(rating)
+        let ratingVal = Array.from({ length: 5 }).fill("✩").join("")
+        for (let i = 0; i <= customer.rating; i++) {
+            ratingVal = ratingVal.replace("✩", "⭐️")
+        }
+        rating.textContent = ratingVal
+        reviewerImageDiv.appendChild(reviewerImage)
+        reviewer.appendChild(reviewerImageDiv)
 
-    testomonial.appendChild(tilda);
-    testomonial.appendChild(content);
-    testomonial.appendChild(clientDiv);
-    tilda.appendChild(imgTilda);
-    content.textContent = customer.content;
-    clientDiv.appendChild(reviewer);
-    clientDiv.appendChild(rating);
-    let ratingVal = Array.from({ length: 5 }).fill("✩").join("");
-    for (let i = 0; i <= customer.rating; i++) {
-      ratingVal = ratingVal.replace("✩", "⭐️");
-    }
-    rating.textContent = ratingVal;
-    reviewerImageDiv.appendChild(reviewerImage);
-    reviewer.appendChild(reviewerImageDiv);
+        reviewer.appendChild(info)
+        info.appendChild(customersName)
+        info.appendChild(customerRole)
 
-    reviewer.appendChild(info);
-    info.appendChild(customersName);
-    info.appendChild(customerRole);
-
-    swiperWrapper.appendChild(testomonial);
-  });
-})();
+        swiperWrapper.appendChild(testomonial)
+    })
+})()
