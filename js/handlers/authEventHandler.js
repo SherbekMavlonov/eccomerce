448a55892c2e3ac519677b78851a6befe7c9fea4
Bakhriddin <bakhriddin.c11@gmail.com>
2023-09-03 18:25:18 +0500

    updated the folder structure

--- js/handlers/authEventHandler.js
+++ js/handlers/authEventHandler.js
@@ -1,5 +1,5 @@
-import Auth from './components/Auth.js';
-import { authSelectors as HTML } from './domSelectors.js';
+import Auth from '../components/Auth.js';
+import { authSelectors as HTML } from '../domSelectors.js';
 
 const auth = new Auth()
 
@@ -12,35 +12,38 @@ HTML.createAccountBtnEl.addEventListener('click', (e) => {
     HTML.createPasswordRepeatEl.value)
 })
 
+
 // Login
 HTML.loginBtnEl.addEventListener('click', (e) => {
   e.preventDefault()
-  auth.login(HTML.loginEmailEl.value, HTML.loginPasswordEl.value)
+  auth.login(
+    HTML.loginEmailEl.value,
+    HTML.loginPasswordEl.value)
 })
 
-// Navigate to Log In
+
+// Navigate to "Log In"
 HTML.loginLinkEl.addEventListener('click', () => {
   HTML.createAccountContainerEl.classList.add('hide')
   HTML.loginContainerEl.classList.remove('hide')
 })
 
-// Navigate to Create Account
+
+// Navigate to "Create Account"
 HTML.createAccountLinkEl.addEventListener('click', () => {
   HTML.createAccountContainerEl.classList.remove('hide')
   HTML.loginContainerEl.classList.add('hide')
 })
 
+
 // Hide or Show Password
 HTML.eyeIconsEl.forEach((icon, index) => {
   icon.addEventListener('click', () => {
-    if (icon.classList.contains('bx-hide')) {
-      icon.classList.remove('bx-hide')
-      icon.classList.add('bx-show')
-      HTML.eyeInputEl[index].type = "text"
-    } else {
-      icon.classList.add('bx-hide')
-      icon.classList.remove('bx-show')
-      HTML.eyeInputEl[index].type = "password"
-    }
+    const isHidden = icon.classList.contains('bx-hide')
+    const oldClass = isHidden ? 'bx-hide' : 'bx-show'
+    const newClass = isHidden ? 'bx-show' : 'bx-hide'
+
+    icon.classList.replace(oldClass, newClass)
+    HTML.eyeInputEl[index].type = isHidden ? 'text' : 'password'
   })
 })
