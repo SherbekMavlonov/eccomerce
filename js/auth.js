dd80f9fd5ae4058b8b8dede9899962f2867fdca1
Bakhriddin <bakhriddin.c11@gmail.com>
2023-09-03 17:23:41 +0500

    Auth basic funcionality added

--- js/auth.js
+++ js/auth.js
@@ -1,135 +1,46 @@
-const loginForm = document.querySelector(".loginForm"),
-    loginEmailInput = document.querySelector(".loginEmail"),
-    loginPasswordInput = document.querySelector(".loginPassword"),
-    lWarning = document.querySelector("#login .warning"),
-    lerror = document.querySelector("#login .error"),
-    lsuccess = document.querySelector("#login .success"),
-    lValidatorPassword = document.querySelector("#login .validatorPassword"),
-    lValidatorEmail = document.querySelector("#login .validatorEmail"),
-    lPassHide = document.querySelector("#login .hide"),
-    lPassShow = document.querySelector("#login .show")
+import Auth from './components/Auth.js';
+import { authSelectors as HTML } from './domSelectors.js';
 
-let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
+const auth = new Auth()
 
-loginForm.addEventListener("submit", (e) => {
-    e.preventDefault()
-    const userEmail = loginEmailInput.value
-    const userPassword = loginPasswordInput.value
+// Create Account
+HTML.createAccountBtnEl.addEventListener('click', (e) => {
+  e.preventDefault()
+  auth.createAccount(
+    HTML.createEmailEl.value,
+    HTML.createPasswordEl.value,
+    HTML.createPasswordRepeatEl.value)
+})
 
-    const userData = { email: userEmail, password: userPassword }
+// Login
+HTML.loginBtnEl.addEventListener('click', (e) => {
+  e.preventDefault()
+  auth.login(HTML.loginEmailEl.value, HTML.loginPasswordEl.value)
+})
 
-    localStorage.setItem("userData", JSON.stringify(userData))
+// Navigate to Log In
+HTML.loginLinkEl.addEventListener('click', () => {
+  HTML.createAccountContainerEl.classList.add('hide')
+  HTML.loginContainerEl.classList.remove('hide')
+})
 
-    const validationMessage = validatePassword(userPassword)
+// Navigate to Create Account
+HTML.createAccountLinkEl.addEventListener('click', () => {
+  HTML.createAccountContainerEl.classList.remove('hide')
+  HTML.loginContainerEl.classList.add('hide')
+})
 
-    if (validationMessage === "Parol tekshirilgan va qabul qilindi.") {
+// Hide or Show Password
+HTML.eyeIconsEl.forEach((icon, index) => {
+  icon.addEventListener('click', () => {
+    if (icon.classList.contains('bx-hide')) {
+      icon.classList.remove('bx-hide')
+      icon.classList.add('bx-show')
+      HTML.eyeInputEl[index].type = "text"
     } else {
-        console.log(validationMessage)
+      icon.classList.add('bx-hide')
+      icon.classList.remove('bx-show')
+      HTML.eyeInputEl[index].type = "password"
     }
-})
-
-window.addEventListener("load", () => {
-    const storedUserData = localStorage.getItem("userData")
-    if (storedUserData) {
-        const userData = JSON.parse(storedUserData)
-        loginEmailInput.value = userData.email
-    }
-})
-
-loginEmailInput.addEventListener("input", () => {
-    lValidatorEmail.classList.remove("dHide")
-    if (loginEmailInput.value.match(pattern)) {
-        lValidatorEmail.textContent = "Valid Email"
-        lValidatorEmail.classList.remove("warning")
-        lValidatorEmail.classList.add("success-color")
-    } else {
-        lValidatorEmail.textContent = "Invalid Email"
-        lValidatorEmail.classList.remove("success-color")
-        lValidatorEmail.classList.add("warning")
-    }
-})
-
-loginPasswordInput.addEventListener("input", () => {
-    const userPassword = loginPasswordInput.value
-    const validationMessage = validatePassword(userPassword)
-    lValidatorPassword.style.color = "red"
-
-    if (validationMessage === "Parol tekshirilgan va qabul qilindi.") {
-        lValidatorPassword.textContent = ""
-    } else {
-        lValidatorPassword.textContent = validationMessage
-    }
-})
-
-function validatePassword(password) {
-    const minLength = 8
-    const maxLength = 16
-    const hasUpperCase = /[A-Z]/.test(password)
-    const hasLowerCase = /[a-z]/.test(password)
-    const hasDigit = /\d/.test(password)
-    const hasSpecialChar = /[!@#$%^&*()_+{}\[\]:;<>,.?~\-]/.test(password)
-
-    if (password.length < minLength || password.length > maxLength) {
-        return "Parol uzunligi 8 dan 16 belgiga to'g'ri kelishi kerak."
-    }
-
-    if (!hasUpperCase || !hasLowerCase || !hasDigit || !hasSpecialChar) {
-        return "Parol katta harf, kichik harf, raqam va maxsus belgini o'z ichiga olishi kerak."
-    }
-
-    return "Parol tekshirilgan va qabul qilindi."
-}
-
-lPassHide.addEventListener("click", () => {
-    loginPasswordInput.setAttribute("type", "password")
-    lPassHide.classList.add("dHide")
-    lPassShow.classList.remove("dHide")
-})
-lPassShow.addEventListener("click", () => {
-    loginPasswordInput.setAttribute("type", "text")
-    lPassShow.classList.add("dHide")
-    lPassHide.classList.remove("dHide")
-})
-
-/* =====>>  Create Accaunt  <<===== */
-
-const createEmailInput = document.querySelector("#createAccaunt .createEmail")
-const createEmailError = document.querySelector("#createAccaunt .validatorEmail")
-
-createEmailInput.addEventListener("input", function () {
-    const email = createEmailInput.value.trim()
-    const isGmail = email.endsWith("@gmail.com")
-
-    if (!isGmail) {
-        createEmailError.textContent = "Please enter a valid Gmail address."
-        createEmailError.classList.remove("dHide")
-        createEmailError.classList.remove("success-color")
-    } else {
-        createEmailError.textContent = "Valid Email"
-        createEmailError.classList.add("success-color")
-    }
-})
-
-const createPasswordInput = document.querySelector(".createPassword")
-const createPasswordError = createPasswordInput.nextElementSibling
-
-createPasswordInput.addEventListener("input", function () {
-    const password = createPasswordInput.value
-
-    const isLengthValid = password.length >= 8 && password.length <= 16
-    const hasUppercase = /[A-Z]/.test(password)
-    const hasLowercase = /[a-z]/.test(password)
-    const hasNumber = /[0-9]/.test(password)
-
-    if (!isLengthValid || !hasUppercase || !hasLowercase || !hasNumber) {
-        createPasswordError.textContent =
-            "Password must be 8 to 16 characters long and include uppercase, lowercase, and numeric characters."
-        createPasswordError.classList.remove("dHide")
-        createPasswordError.classList.add("error")
-        createPasswordError.classList.remove("success-color")
-    } else {
-        createPasswordError.textContent = "Valid Password"
-        createPasswordError.classList.remove("error")
-        createPasswordError.classList.add("success-color")
-    }
+  })
 })
