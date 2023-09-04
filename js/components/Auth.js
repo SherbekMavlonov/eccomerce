162b18524cd129b5bf62d29cb38b7687af2c4d02
Bakhriddin <bakhriddin.c11@gmail.com>
2023-09-04 10:30:23 +0500

    Basic auth part completed

--- js/components/Auth.js
+++ js/components/Auth.js
@@ -6,11 +6,50 @@ class Auth {
   }
 
   createAccount(email, password, repeatPassword) {
-    useStorage.setItem('users', JSON.stringify({ email, password }))
+    const [isValid, errorMessage] = this.formValidator(email, password, repeatPassword)
+
+    if (isValid) {
+      useStorage.setItem('users', JSON.stringify(
+        { email, password, loggedIn: true }
+      ))
+      this.navigate('/pages/dashboard.html')
+    } else {
+      document.querySelector('.create-account--error').textContent = errorMessage
+    }
   }
 
   login(email, password) {
-    console.log(users)
+    if (email === this.users.email && password === this.users.password) {
+      this.navigate('/pages/dashboard.html')
+    } else {
+      document.querySelector('.login--error').textContent = 'Email or password is incorrect'
+    }
+  }
+
+  formValidator(email, password, repeatPassword) {
+    let status = true;
+    let error = '';
+
+    if (email.length < 5 || !email.includes('@')) {
+      status = false
+      error = 'invalid email'
+    }
+
+    if (password.length < 4) {
+      status = false
+      error = 'Password: Must be at least 4 characters'
+    }
+
+    if (password !== repeatPassword) {
+      status = false;
+      error = 'The second password was not correct';
+    }
+
+    return [status, error]
+  }
+
+  navigate(url) {
+    window.location.href = `${url}`;
   }
 }
 
